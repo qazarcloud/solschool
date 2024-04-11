bd987ef5dfe507ade6a2ee022a3f75312179c4eb
damienqazar <root@CELLO>
2024-04-12 00:05:55 +0200

    damn json works dawg

--- midl/src/client.ts
+++ midl/src/client.ts
@@ -1,30 +1,30 @@
 // client.ts
-import { treaty } from '@elysiajs/eden';
+import { edenFetch } from '@elysiajs/eden';
 import type { App } from './index';
 
-const tapp = treaty<App>('localhost:8080');
+const tapp = edenFetch<App>('localhost:8080');
 
-const account = {
-    address: '123',
-    crypto: 'SOL',
-    network: 'mainnet',
-    metadata: {}
-};
-
-// Use async/await to handle the asynchronous nature of the request
-const postData = async () => {
-    try {
-        // Send a POST request to the server with the account data
-        const { data } = await tapp.json.post(account);
-
-        // Log the response data
-        console.log(data);
-    } catch (error) {
-        // Handle any errors that occur during the request
-        console.error('Error:', error);
+const { data } = await tapp('/account/:address', {
+    params: {
+        address: '81meSJqk6SmbQmxdeHt1YEAKx7UrM8njQq1KyCTofeb1'
     }
-};
+})
+console.log(data)
 
-// Call the postData function to initiate the request
-postData();
+// const accountCreate = async () => {
+//     try {
+//         const account = {
+//             address: '123',
+//             crypto: 'SOL',
+//             network: 'mainnet',
+//             metadata: {}
+//         };
+//         const { data } = await tapp.account.post(account);
+//         console.log(data);
+//     } catch (error) {
+//         console.error('Error:', error);
+//     }
+// };
+
+// accountCreate();
 
--- midl/src/index.ts
+++ midl/src/index.ts
@@ -1,5 +1,16 @@
 import { Elysia, t } from 'elysia'
+import { Client, cacheExchange, fetchExchange, gql } from '@urql/core';
 
+const client = new Client({
+  url: 'https://solschool.hasura.app/v1/graphql',
+  exchanges: [cacheExchange, fetchExchange],
+  fetchOptions: () => {
+	const token = 'cj0Ondu0kPfqUi86mfh826X5ZmdEv6ybFPvR5GCFQD5ILIiikfw5hFeDimjf6Qsg'
+    return {
+      headers: { 'x-hasura-admin-secret': token ? `${token}` : '' },
+    };
+  },
+});
 const t1 = performance.now()
 
 const loggerPlugin = new Elysia()
@@ -15,28 +26,42 @@ const app = new Elysia()
 		}
 	})
 	.use(loggerPlugin)
-	.state('build', Date.now())
-	.get('/', () => 'SolSchool')
-	// .get('/json', () => ({
-	// 	hi: 'world'
-	// }))
-	.get('/root/plugin/log', ({ log, store: { build } }) => {
-		log()
-
-		return build
-	})
-	.get('/wildcard/*', () => 'Hi Wildcard')
-	.get('/query', () => 'Elysia', {
-		beforeHandle: ({ query }) => {
-			console.log('Name:', query?.name)
-
-			if (query?.name === 'aom') return 'Hi saltyaom'
-		},
-		query: t.Object({
-			name: t.String()
+	.get(
+		'/account/:address', 
+		({ params: { address } }) => {
+			const GetAccount = gql`
+				query GetAccount($address: String!) {
+					account(where: {address: {_eq: $address}}) {
+						address
+						crypto
+						id
+						metadata
+						network
+						uuid
+					}
+				}			  
+			`;
+			console.log(GetAccount)
+			const account = client
+			.query(GetAccount, { address: address })
+			.toPromise()
+			.then((result) => {
+			  console.log(result.data.account[0]);
+			  return result.data.account[0];
+			});
+			return account
+		},{
+			params: t.Object({
+				address: t.String()
+			}),
+			// body: t.Object({
+			// 	address: t.String(),
+			// 	crypto: t.String(),
+			// 	network: t.String(),
+			// 	metadata: t.Unknown()
+			// })
 		})
-	})
-	.post('/json', async ({ body }) => body, {
+	.post('/account', async ({ body }) => body, {
 		body: t.Object({
 			address: t.String(),
 			crypto: t.String(),
@@ -44,71 +69,6 @@ const app = new Elysia()
 			metadata: t.Unknown()
 		})
 	})
-	.post('/transform-body', async ({ body }) => body, {
-		beforeHandle: (ctx) => {
-			ctx.body = {
-				...ctx.body,
-				additional: 'Elysia'
-			}
-		},
-		body: t.Object({
-			name: t.String(),
-			additional: t.String()
-		})
-	})
-	.get('/id/:id', ({ params: { id } }) => id, {
-		transform({ params }) {
-			params.id = +params.id
-		},
-		params: t.Object({
-			id: t.Number()
-		})
-	})
-	.post('/new/:id', async ({ body, params }) => body, {
-		params: t.Object({
-			id: t.Number()
-		}),
-		body: t.Object({
-			username: t.String()
-		})
-	})
-	.get('/trailing-slash', () => 'A')
-	.group('/group', (app) =>
-		app
-			.onBeforeHandle<{
-				query: {
-					name: string
-				}
-			}>(({ query }) => {
-				if (query?.name === 'aom') return 'Hi saltyaom'
-			})
-			.get('/', () => 'From Group')
-			.get('/hi', () => 'HI GROUP')
-			.get('/elysia', () => 'Welcome to Elysian Realm')
-			.get('/fbk', () => 'FuBuKing')
-	)
-	.get('/response-header', ({ set }) => {
-		set.status = 404
-		set.headers['a'] = 'b'
-
-		return 'A'
-	})
-	.get('/this/is/my/deep/nested/root', () => 'Hi')
-	.get('/build', ({ store: { build } }) => build)
-	.get('/ref', ({ date }) => date())
-	.get('/response', () => new Response('Hi'))
-	.get('/error', () => new Error('Something went wrong'))
-	.get('/401', ({ set }) => {
-		set.status = 401
-
-		return 'Status should be 401'
-	})
-	.get('/timeout', async () => {
-		await new Promise((resolve) => setTimeout(resolve, 2000))
-
-		return 'A'
-	})
-	.all('/all', () => 'hi')
 	.onError(({ code, error, set }) => {
 		if (code === 'NOT_FOUND') {
 			set.status = 404
