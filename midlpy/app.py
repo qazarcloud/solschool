2feb6da27a3a9256d3112dfb454dfa2dc68dae33
root <root@Astrale>
2024-04-11 12:59:40 +0200

    + midlpy

---
+++ midlpy/app.py
@@ -0,0 +1,54 @@
+from litestar import Litestar, get
+# from httpx import AsyncClient
+from gql import gql, Client
+from gql.transport.httpx import HTTPXAsyncTransport
+
+transport = HTTPXAsyncTransport(
+    url="https://solschool.hasura.app/v1/graphql",
+    headers={
+        'accept: */*',
+        'accept-language: en-US,en;q=0.8',
+        'content-type: application/json',
+        'origin: https://cloud.hasura.io',
+        'referer: https://cloud.hasura.io/',
+        'sec-ch-ua: "Brave";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
+        'sec-ch-ua-mobile: ?0',
+        'sec-ch-ua-platform: "Windows"',
+        'sec-fetch-dest: empty',
+        'sec-fetch-mode: cors',
+        'sec-fetch-site: cross-site',
+        'sec-gpc: 1',
+        'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+        'x-hasura-admin-secret: cj0Ondu0kPfqUi86mfh826X5ZmdEv6ybFPvR5GCFQD5ILIiikfw5hFeDimjf6Qsg',
+    }
+)
+
+client = Client(transport=transport, fetch_schema_from_transport=True)
+
+@get("/account/{wallet:str}")
+async def get_account(wallet: str) -> dict[str, id]:
+    async with Client(
+        transport=transport,
+        fetch_schema_from_transport=True,
+    ) as session:
+        # Execute single query
+        query = gql(
+            """
+            query Account($address: String!) {
+                account(where: { address: { _eq: $address } }) {
+                    address
+                    crypto
+                    id
+                    metadata
+                    network
+                    uuid
+                }
+            }
+            """
+        )
+        result = await session.execute(query, variable_values={'address': wallet})
+        print(result)
+        return {"account": result}
+
+
+app = Litestar([get_account])
